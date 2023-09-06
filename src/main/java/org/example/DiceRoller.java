58074aec7d52c7a1f2605405898ec7b1f2162f54
AndrewSwinney <andrewswinney14@gmail.com>
2023-09-06 16:56:54 +0100

    Added a counter to DiceRoller app

--- src/main/java/org/example/DiceRoller.java
+++ src/main/java/org/example/DiceRoller.java
@@ -1,20 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class DiceRoller {
     public static void main(String[] args) {
 
         Dice player1 = new Dice("player1");
         Dice player2 = new Dice("player2");
-        int roll1 = player1.roll();
-        int roll2 = player2.roll();
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Welcome to Dice Roller!");
+        System.out.println("Would you like to play: Yes/No ?");
+        String playGame = myScanner.nextLine();
 
-        
-        while (roll1 != roll2) {
-            roll2 = player2.roll();
-        }
-        if (roll2 == roll1) {
-            System.out.print("You have a match!");
-        }
+        if (playGame.equalsIgnoreCase("yes")) {
+            int roll1 = player1.roll();
+            int roll2 = player2.roll();
 
+            int counter = 1;
+
+            while (roll1 != roll2) {
+                roll2 = player2.roll();
+                counter = counter + 1;
+            }
+            if (roll2 == roll1) {
+                System.out.println("You have a match!");
+                System.out.println("It took " + counter + " " + "roll(s)!");
+            } else if (playGame.equalsIgnoreCase("no")) {
+                System.out.println("No problem, have a nice day.");
+            }
+        }
     }
 }
